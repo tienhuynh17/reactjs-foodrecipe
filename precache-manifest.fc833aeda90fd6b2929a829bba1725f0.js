0f1f9aaaea1099bef368a0477adc802aa9d727f9
tienzlatan <39383420+tienzlatan@users.noreply.github.com>
2020-06-18 01:37:16 +0700

    Updates

---
+++ precache-manifest.fc833aeda90fd6b2929a829bba1725f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "093c3d1973fed5e6f0d7ea50ab29a71b",
+    "url": "/reactjs-foodrecipe/index.html"
+  },
+  {
+    "revision": "68334c73a09a92dbcd94",
+    "url": "/reactjs-foodrecipe/static/css/main.9b65d12e.chunk.css"
+  },
+  {
+    "revision": "7185cabcb21268d26deb",
+    "url": "/reactjs-foodrecipe/static/js/2.508730c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-foodrecipe/static/js/2.508730c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68334c73a09a92dbcd94",
+    "url": "/reactjs-foodrecipe/static/js/main.ae3d78e1.chunk.js"
+  },
+  {
+    "revision": "1f11946e6f0b5f5d1e0c",
+    "url": "/reactjs-foodrecipe/static/js/runtime-main.6ebfdefd.js"
+  }
+]);
